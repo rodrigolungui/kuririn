9076882b35b00fa76a461540ffe1f954461d139c
Rodrigo Lungui <lungui.rodrigo@gmail.com>
2014-12-08 16:26:56 -0200

    Add task for jasmine tests

--- gruntfile.js
+++ gruntfile.js
@@ -93,5 +93,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   
   grunt.registerTask('default', ['connect:server', 'watch']);
+  grunt.registerTask('test', ['jasmine']);
 
 };
